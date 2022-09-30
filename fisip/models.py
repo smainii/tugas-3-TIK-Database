7b7265a49e16effbb08712de32ee5d051adea3fb
unknown <sitimashurula@gmail.com>
2022-09-30 17:07:48 +0700

    first commit

--- fisip/models.py
+++ fisip/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Nip(models.Model):
+    nama = models.CharField(max_length=50)
+    keterangan = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
 class Biodatadosenfisip(models.Model):
-    nip = models.IntegerField()
+    nip_id = models.ForeignKey(Nip, on_delete=models.CASCADE, null=True)
     nama = models.CharField(max_length=100)
     tanggal_lahir = models.DateField()
     photo = models.ImageField()
@@ -15,7 +22,7 @@ class Biodatadosenfisip(models.Model):
         return self.nama
 
 class Biodatatendikfisip(models.Model):
-    nip = models.IntegerField()
+    nip_id = models.ForeignKey(Nip, on_delete=models.CASCADE, null=True)
     nama = models.CharField(max_length=100)
     tanggal_lahir = models.DateField()
     photo = models.ImageField()

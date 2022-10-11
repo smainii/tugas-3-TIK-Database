c81dddb87d934e4eb7ed4d6aa036f059ce437057
unknown <sitimashurula@gmail.com>
2022-10-11 22:01:58 +0700

    k

--- fk/models.py
+++ fk/models.py
@@ -1,6 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Fakultas(models.Model):
+    nama = models.CharField(max_length=50)
+    keterangan = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
+class Jurusan(models.Model):
+    nama = models.CharField(max_length=50)
+    keterangan = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
+class MatKul(models.Model):
+    nama = models.CharField(max_length=50)
+    keterangan = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
+class Kelas(models.Model):
+    nama = models.CharField(max_length=50)
+    keterangan = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
 class Nip(models.Model):
     nama = models.CharField(max_length=50)
     keterangan = models.TextField(null=True)
@@ -8,12 +36,61 @@ class Nip(models.Model):
     def __str__(self):
         return self.nama
 
-class Biodatadosenfk(models.Model):
+class Nim(models.Model):
+    nama = models.CharField(max_length=50)
+    keterangan = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
+class Fakultas_fk(models.Model):
+    kode_fakul = models.ForeignKey(Fakultas, on_delete=models.CASCADE, null=True)
+    nama = models.CharField(max_length=100)
+    tanggal_berdiri = models.DateField()
+    email = models.CharField(max_length=100)
+    telepon = models.CharField(max_length=100)
+    alamat = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
+class Jurusan_fk(models.Model):
+    kode_jurusan = models.ForeignKey(Jurusan, on_delete=models.CASCADE, null=True)
+    nama = models.CharField(max_length=100)
+    tanggal_berdiri = models.DateField()
+    email = models.CharField(max_length=100)
+    telepon = models.CharField(max_length=100)
+    alamat = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
+class MatKul_fk(models.Model):
+    kode_MatKul = models.ForeignKey(MatKul, on_delete=models.CASCADE, null=True)
+    nama = models.CharField(max_length=100)
+    jurusan = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nama
+
+class Perkuliahan_fk(models.Model):
+    kode_ruangan = models.ForeignKey(Kelas, on_delete=models.CASCADE, null=True)
+    nama_ruangan = models.CharField(max_length=100)
+    jurusan = models.CharField(max_length=50)
+    dosen = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nama_ruangan
+
+class Biodata_dosen_fk(models.Model):
     nip_id = models.ForeignKey(Nip, on_delete=models.CASCADE, null=True)
     nama = models.CharField(max_length=100)
     tanggal_lahir = models.DateField()
+    jenis_kelamin = models.CharField(max_length=20, null=True)
+    agama = models.CharField(max_length=20, null=True)
     photo = models.ImageField()
     email = models.CharField(max_length=100)
+    telepon = models.CharField(max_length=100, null=True)
     fakultas = models.CharField(max_length=100)
     prodi = models.CharField(max_length=50)
     alamat = models.TextField(null=True)
@@ -21,27 +98,57 @@ class Biodatadosenfk(models.Model):
     def __str__(self):
         return self.nama
 
-class Biodatatendikfk(models.Model):
+class User_dosen_fk(models.Model):
+    nip_id = models.ForeignKey(Nip, on_delete=models.CASCADE, null=True)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.username
+
+class Biodata_tendik_fk(models.Model):
     nip_id = models.ForeignKey(Nip, on_delete=models.CASCADE, null=True)
     nama = models.CharField(max_length=100)
     tanggal_lahir = models.DateField()
+    jenis_kelamin = models.CharField(max_length=20, null=True)
+    agama = models.CharField(max_length=20, null=True)
     photo = models.ImageField()
     email = models.CharField(max_length=100)
+    telepon = models.CharField(max_length=100, null=True)
     unit = models.CharField(max_length=100)
     alamat = models.TextField(null=True)
 
     def __str__(self):
         return self.nama
 
-class Biodatamahasiswafk(models.Model):
-    nim = models.IntegerField()
+class User_tendik_fk(models.Model):
+    nip_id = models.ForeignKey(Nip, on_delete=models.CASCADE, null=True)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.username
+
+class Biodata_mahasiswa_fk(models.Model):
+    nim = models.ForeignKey(Nim, on_delete=models.CASCADE, null=True)
     nama = models.CharField(max_length=100)
     tanggal_lahir = models.DateField()
+    jenis_kelamin = models.CharField(max_length=20, null=True)
+    agama = models.CharField(max_length=20, null=True)
     photo = models.ImageField()
     email = models.CharField(max_length=100)
+    telepon = models.CharField(max_length=100, null=True)
     fakultas = models.CharField(max_length=100)
     prodi = models.CharField(max_length=50)
     alamat = models.TextField(null=True)
 
     def __str__(self):
         return self.nama
+
+class User_mahasiswa_fk(models.Model):
+    nim_id = models.ForeignKey(Nim, on_delete=models.CASCADE, null=True)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.username
--- fkip/models.py
+++ fkip/models.py
@@ -1,6 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Fakultas(models.Model):
+    nama = models.CharField(max_length=50)
+    keterangan = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
+class Jurusan(models.Model):
+    nama = models.CharField(max_length=50)
+    keterangan = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
+class MatKul(models.Model):
+    nama = models.CharField(max_length=50)
+    keterangan = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
+class Kelas(models.Model):
+    nama = models.CharField(max_length=50)
+    keterangan = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
 class Nip(models.Model):
     nama = models.CharField(max_length=50)
     keterangan = models.TextField(null=True)
@@ -8,12 +36,61 @@ class Nip(models.Model):
     def __str__(self):
         return self.nama
 
-class Biodatadosenfkip(models.Model):
+class Nim(models.Model):
+    nama = models.CharField(max_length=50)
+    keterangan = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
+class Fakultas_fkip(models.Model):
+    kode_fakul = models.ForeignKey(Fakultas, on_delete=models.CASCADE, null=True)
+    nama = models.CharField(max_length=100)
+    tanggal_berdiri = models.DateField()
+    email = models.CharField(max_length=100)
+    telepon = models.CharField(max_length=100)
+    alamat = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
+class Jurusan_fkip(models.Model):
+    kode_jurusan = models.ForeignKey(Jurusan, on_delete=models.CASCADE, null=True)
+    nama = models.CharField(max_length=100)
+    tanggal_berdiri = models.DateField()
+    email = models.CharField(max_length=100)
+    telepon = models.CharField(max_length=100)
+    alamat = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama
+
+class MatKul_fkip(models.Model):
+    kode_MatKul = models.ForeignKey(MatKul, on_delete=models.CASCADE, null=True)
+    nama = models.CharField(max_length=100)
+    jurusan = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nama
+
+class Perkuliahan_fkip(models.Model):
+    kode_ruangan = models.ForeignKey(Kelas, on_delete=models.CASCADE, null=True)
+    nama_ruangan = models.CharField(max_length=100)
+    jurusan = models.CharField(max_length=50)
+    dosen = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nama_ruangan
+
+class Biodata_dosen_fkip(models.Model):
     nip_id = models.ForeignKey(Nip, on_delete=models.CASCADE, null=True)
     nama = models.CharField(max_length=100)
     tanggal_lahir = models.DateField()
+    jenis_kelamin = models.CharField(max_length=20, null=True)
+    agama = models.CharField(max_length=20, null=True)
     photo = models.ImageField()
     email = models.CharField(max_length=100)
+    telepon = models.CharField(max_length=100, null=True)
     fakultas = models.CharField(max_length=100)
     prodi = models.CharField(max_length=50)
     alamat = models.TextField(null=True)
@@ -21,27 +98,57 @@ class Biodatadosenfkip(models.Model):
     def __str__(self):
         return self.nama
 
-class Biodatatendikfkip(models.Model):
+class User_dosen_fkip(models.Model):
+    nip_id = models.ForeignKey(Nip, on_delete=models.CASCADE, null=True)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.username
+
+class Biodata_tendik_fkip(models.Model):
     nip_id = models.ForeignKey(Nip, on_delete=models.CASCADE, null=True)
     nama = models.CharField(max_length=100)
     tanggal_lahir = models.DateField()
+    jenis_kelamin = models.CharField(max_length=20, null=True)
+    agama = models.CharField(max_length=20, null=True)
     photo = models.ImageField()
     email = models.CharField(max_length=100)
+    telepon = models.CharField(max_length=100, null=True)
     unit = models.CharField(max_length=100)
     alamat = models.TextField(null=True)
 
     def __str__(self):
         return self.nama
 
-class Biodatamahasiswafkip(models.Model):
-    nim = models.IntegerField()
+class User_tendik_fkip(models.Model):
+    nip_id = models.ForeignKey(Nip, on_delete=models.CASCADE, null=True)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.username
+
+class Biodata_mahasiswa_fkip(models.Model):
+    nim = models.ForeignKey(Nim, on_delete=models.CASCADE, null=True)
     nama = models.CharField(max_length=100)
     tanggal_lahir = models.DateField()
+    jenis_kelamin = models.CharField(max_length=20, null=True)
+    agama = models.CharField(max_length=20, null=True)
     photo = models.ImageField()
     email = models.CharField(max_length=100)
+    telepon = models.CharField(max_length=100, null=True)
     fakultas = models.CharField(max_length=100)
     prodi = models.CharField(max_length=50)
     alamat = models.TextField(null=True)
 
     def __str__(self):
         return self.nama
+
+class User_mahasiswa_fkip(models.Model):
+    nim_id = models.ForeignKey(Nim, on_delete=models.CASCADE, null=True)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.username
